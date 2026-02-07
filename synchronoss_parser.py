bf662aa565b2ecc8561a8fe15eb792c1341f6717
Jesus M. Alonso <jesusmalonso7@gmail.com>
2026-02-07 15:19:33 -0500

    I have just been testing small changes to see what happens

--- synchronoss_parser.py
+++ synchronoss_parser.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timezone
 from secrets import token_urlsafe
+from collections import Counter
 from typing import List
 import pathlib
 import logging
+import time
 import json
 import sys
 import os
@@ -14,32 +16,6 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s %(message)s',
     stream=sys.stderr
 )
-
-
-def parse_cmd_line(argv: list) -> str:
-    # Command line variable processing
-    if len(argv) < 2:
-        print("Usage: python synchronoss_parser.py [info|run]", file=sys.stderr)
-        sys.exit(2)
-    cmd = argv[1]
-    if cmd == 'info':
-        show_info()
-        sys.exit(0)
-    if cmd == 'run':
-        if len(argv) < 3:
-            print("Please provide the path to the (extracted) Synchronoss return folder: "
-                  "synchronoss_parser.py run <folder path>", file=sys.stderr)
-            sys.exit(2)
-        dir_path = sys.argv[2]
-        if not dir_path:
-            logging.error('Folder path not provided.')
-            sys.exit(1)
-        else:
-            return dir_path
-    else:
-        print("Unknown command. Use 'info' or 'run'.", file=sys.stderr)
-        logging.info("Unknown command. Use 'info' or 'run'.")
-        sys.exit(2)
 
 
 def show_info():
@@ -63,7 +39,6 @@ FORMATS_Z = [
     "%b %d %Y %H:%M:%S %Z",     # Apr 04 2024 00:19:33 UTC
     "%Y-%m-%d %H:%M:%S %Z",     # 2025-12-27 01:16:16 UTC
     "%Y-%m-%d %H:%M:%S",        # 2025-12-27 01:16:16
-
 ]
 
 FORMATS_z = [
@@ -96,6 +71,32 @@ def to_iso_utc(ts: str) -> str:
     raise ValueError(f"Unrecognized date format: {ts}")
 
 
+def parse_cmd_line(argv: list) -> str:
+    # Command line variable processing
+    if len(argv) < 2:
+        print("Usage: python synchronoss_parser.py [info|run]", file=sys.stderr)
+        sys.exit(2)
+    cmd = argv[1]
+    if cmd == 'info':
+        show_info()
+        sys.exit(0)
+    if cmd == 'run':
+        if len(argv) < 3:
+            print("Please provide the path to the (extracted) Synchronoss return folder: "
+                  "synchronoss_parser.py run <folder path>", file=sys.stderr)
+            sys.exit(2)
+        dir_path = sys.argv[2]
+        if not dir_path:
+            logging.error('Folder path not provided.')
+            sys.exit(1)
+        else:
+            return dir_path
+    else:
+        print("Unknown command. Use 'info' or 'run'.", file=sys.stderr)
+        logging.info("Unknown command. Use 'info' or 'run'.")
+        sys.exit(2)
+
+
 def files_need_processing(root_path: str) -> bool:
     """
     :param root_path:
@@ -113,41 +114,90 @@ def files_need_processing(root_path: str) -> bool:
     return False
 
 
-def get_messages(root_path, meta: dict, activities: list):
+def render_message(dir_path, message, integrityId):
+    """
+    Returns a paradigm formatted message
+    :param dir_path:
+    :param message:
+    :param integrityId:
+    :return:
+    """
+    return {
+        "platform": "synchronoss",
+        "platformValues": {
+            "messageType": 'sms' if 'sms' in dir_path else 'mms',
+            "senderId": 'sent' if 'out' in dir_path else 'received',
+        },
+        "dirId": integrityId,
+        "messageId": token_urlsafe(16),
+        "messageType": "message",
+        "msgBody": message,
+        "msgDate": pathlib.Path(dir_path).name,
+
+    }
+
+
+def get_messages(root_path, summary, integrityId, activities):
     """
     :param root_path: The sms/in and sms/out path passed from the main function.
-    :param meta:
+    :param summary:
+    :param integrityId: The integrity id passed from the main function.:
     :param activities:
     :return:
     """
-    # os.walk() starts traversing directories from sms/in or sms/out passed in from the main function.
-    for root_folder, subfolders, filenames in os.walk(os.path.normpath(root_path), topdown=True):
-        for filename in filenames:
-            # Many of the text files are empty. Skip those
-            if filename.endswith('.txt') and os.path.getsize(f'{os.path.join(root_folder, filename)}') > 0:
-                with open(os.path.join(root_folder, filename), 'r', encoding='utf-8') as fd:
-                    msg = fd.read()
-                # Send this batch off for processing in main.
+
+    counts = Counter()
+    start = time.time()
+
+    filenames = os.listdir(root_path)
+
+    for filename in filenames:
+        # Keep track of message type count. pathlib.Path(root_path).parent.name returns either 'sms' or 'mms'
+        counts[pathlib.Path(root_path).parent.name] += 1
+        # Many of the text files are empty. Skip those
+        if filename.endswith('.txt') and os.path.getsize(f'{os.path.join(root_path, filename)}') > 0:
+            with open(os.path.join(root_path, filename), 'r', encoding='utf-8') as fd:
+                msg = fd.read()
+                print(json.dumps({"type": "message", "data": render_message(root_path, msg, integrityId)}, ensure_ascii=False), flush=True)
+
+                # Create activity
                 activities.append({
                     "type": "data",
-                    "dirId": meta["dirId"],
-                    "platform": "Synchronoss",
-                    "date": pathlib.Path(root_folder).name,  # The text files are stored in folders named after the date
-                    "caseId": None,                          # the file was received or sent. e.g. 2024-01-16
-                    "event": 'sms message'
+                    "date": pathlib.Path(root_path).name,
+                    "dirId": integrityId,
+                    "platform": "synchronoss",
+                    "caseId": None,
+                    "event": 'sms' if 'sms' in pathlib.Path(root_path).parent.parent.name else 'mms',
                 })
-                yield {
-                    "platform": "Synchronoss",
-                    "dirId": meta["dirId"],
-                    "dateRun": meta["dateRun"],
-                    "messageId": token_urlsafe(16),
-                    "msgFrom": "Received" if pathlib.Path(root_folder).parent.name == 'in' else "Sent",
-                    "messageType": "sms",
-                    "msgBody": msg,
-                    "fileId": None,
-                    "msgDate": pathlib.Path(root_folder).name,  # same as above
-                    "epoch": None
-                }
+
+    duration = time.time() - start
+
+    summary.append({
+        "file": os.path.basename(root_path),
+        "time_taken_secs": round(duration, 2),
+        "messages": sum(counts.values()),
+        "breakdown": dict(counts)
+    })
+
+
+def render_message(dir_path, message, integrityId):
+    """
+    Returns a paradigm formatted message
+    :param dir_path:
+    :param message:
+    :param integrityId:
+    :return:
+    """
+    return {
+        "platform": "synchronoss",
+        "dirId": integrityId,
+        "messageId": token_urlsafe(16),
+        "msgFrom": 'sent' if 'out' in dir_path else 'received',
+        #"msgFrom": row.get('sender_username'),
+        "messageType": 'sms' if 'sms' in dir_path else 'mms',
+        "msgBody": message,
+        "msgDate": pathlib.Path(dir_path).name,
+    }
 
 
 # Command Line Interface (CLI)
@@ -159,39 +209,27 @@ def main(argv: List[str]) -> int:
     # Variables to store data collected during processing..
     activities = []
     integrityId = token_urlsafe(16)
-    meta = {
-        "dirId": integrityId,
-        "dateRun": datetime.now(timezone.utc).isoformat()
-    }
+    summary = []
 
     # Command line variable processing
     if dir_path := parse_cmd_line(argv):
+        # Prevents os.walk() from processing these directories. These directories do not contain any data of value.
         exclude_dir = ['call', 'VZMOBILE',]
         # Process all the files and folders found in dir_path
         for root_folder, subfolders, filenames in os.walk(os.path.normpath(dir_path), topdown=True):
-            # Exclude directories from the subfolders list to skip traversal. These directories do not contain any data
-            # of value but do contain numerous subdirectories that would be traversed for no useful reason.
+            # Exclude directories based on exclude_dir list above.
             subfolders[:] = [d for d in subfolders if d not in exclude_dir]
 
-            if 'sms' in root_folder:
-                if 'in' == pathlib.Path(root_folder).name and files_need_processing(root_folder):
-                    for record in get_messages(root_folder, meta, activities):
-                        print(json.dumps({"type": "message", "data": record, "dirId": integrityId},
-                                         ensure_ascii=False), flush=True)
-                elif 'out' == pathlib.Path(root_folder).name and files_need_processing(root_folder):
-                    for record in get_messages(root_folder, meta, activities):
-                        print(json.dumps({"type": "message", "data": record, "dirId": integrityId},
-                                         ensure_ascii=False), flush=True)
+            if 'sms' in root_folder or 'mms' in root_folder:
+                if 'in' == pathlib.Path(root_folder).parent.name:
+                    if 'in' == pathlib.Path(root_folder).parent.name:
+                        if files_need_processing(root_folder):
+                            get_messages(root_folder, summary, integrityId, activities)
+                if 'out' == pathlib.Path(root_folder).parent.name:
+                        if 'out' == pathlib.Path(root_folder).parent.name:
+                            if files_need_processing(root_folder):
+                                get_messages(root_folder, summary, integrityId, activities)
 
-            elif 'mms' in root_folder:
-                if 'in' == pathlib.Path(root_folder).name and files_need_processing(root_folder):
-                    for record in get_messages(root_folder, meta, activities):
-                        print(json.dumps({"type": "message", "data": record, "dirId": integrityId},
-                                         ensure_ascii=False), flush=True)
-                elif 'out' == pathlib.Path(root_folder).name and files_need_processing(root_folder):
-                    for record in get_messages(root_folder, meta, activities):
-                        print(json.dumps({"type": "message", "data": record, "dirId": integrityId},
-                                         ensure_ascii=False), flush=True)
         # Print final Summary
         print(json.dumps({"type": "plugin_summary", "data": {
             "company": "Synchronoss",
